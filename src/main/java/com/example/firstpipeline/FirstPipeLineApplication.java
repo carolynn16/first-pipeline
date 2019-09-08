7c4704457b8bc51cb610b7e0983144b226087518
Mya <nightmareangel6@gmail.com>
2019-09-08 10:02:42 +0630

    first

---
+++ src/main/java/com/example/firstpipeline/FirstPipeLineApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPipeLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstPipeLineApplication.class, args);
+    }
+
+}
