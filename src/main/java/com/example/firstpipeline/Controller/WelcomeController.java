2afac5bbfb02f888c3a2b529bc2a90fad1f77003
Mya <nightmareangel6@gmail.com>
2019-09-08 10:24:50 +0630

    second commit

--- src/main/java/com/example/firstpipeline/Controller/WelcomeController.java
+++ src/main/java/com/example/firstpipeline/Controller/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping("/")
     public String welcome(){
-        return "Hello Aws Pipeline";
+        return "Hello Aws Pipeline ! Happy Coding...";
     }
 }
 
